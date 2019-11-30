6f221ab3ff6ad0b41c6615b553c6eaaf9a892124
Chris Scott <cyberstrike@gmail.com>
2019-11-30 18:28:06 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home bg-primary">
+    <div class="container-fluid ">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+    </div>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
